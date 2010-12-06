7226bdbc88e36e3ec89f170938f72e7ff32e702b
Victor Sokolov <gzigzigzeo@gmail.com>
2010-12-06 16:20:43 +0300

    paths fix

--- lib/admin-helpers/railtie.rb
+++ lib/admin-helpers/railtie.rb
@@ -1,8 +1,7 @@
 module AdminHelpers
-  class Railtie < ::Rails::Railtie
-    initializer "admin_helpers.on_controller" do |app|
-      paths.app << "app"
-      
+  class Railtie < ::Rails::Engine
+    paths.app << "app"    
+    initializer "admin_helpers.on_controller" do |app|    
       ActiveSupport.on_load :action_controller do
         ActionController::Base.helper(AdminHelpers::Helper)      
       end  
