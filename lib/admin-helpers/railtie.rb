26e92ab45c49d991ca57b032fb497a948882d9ff
Victor Sokolov <gzigzigzeo@gmail.com>
2010-12-06 16:25:50 +0300

    asset_path

--- lib/admin-helpers/railtie.rb
+++ lib/admin-helpers/railtie.rb
@@ -1,10 +1,17 @@
 module AdminHelpers
   class Railtie < ::Rails::Engine
     paths.app << "app"    
-    initializer "admin_helpers.on_controller" do |app|    
+    config.asset_path = "/admin/%s"
+    engine_name "admin-helpers"
+
+    initializer "admin-helpers.on_controller" do |app|    
       ActiveSupport.on_load :action_controller do
         ActionController::Base.helper(AdminHelpers::Helper)      
       end  
     end
+    
+#    rake_tasks do
+#      load "admin-helpers.tasks"
+#    end    
   end
 end
