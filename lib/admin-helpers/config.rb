c0844ea7149fceff8aa562bc04bb542ba100850f
Victor Sokolov <gzigzigzeo@gmail.com>
2011-02-04 18:36:20 +0300

    fixes for ck

--- lib/admin-helpers/config.rb
+++ lib/admin-helpers/config.rb
@@ -3,6 +3,6 @@ module AdminHelpers
   self.config = {
     :logo => "/images/admin/molinos.png",
     :logo_size => "104x20",    
-    :javascripts => ['jquery-ui', 'ckeditor/ckeditor.js', 'ckeditor/adapters/jquery', 'admin', 'jquery.ui.datepicker-ru', 'jquery.form']
+    :ckeditor_js => ['ckeditor/ckeditor.js', 'ckeditor/adapters/jquery']
   }
 end
